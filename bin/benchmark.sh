7a5859d69fbbe76187ecdfed73617e684b84addc
Dan Collis-Puro <dan@collispuro.net>
2015-08-10 15:55:09 -0400

    Use different files for each size test, clean up more aggressively

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -20,8 +20,11 @@ if [ ! -d "$filesystem" ]; then
   exit 1
 fi
 
-test_root="$filesystem/bonnie_tests"
+test_root="$filesystem/filesystem_tests_tmp"
+
+rm -Rf "$test_root"
 mkdir -p "$test_root"
+
 trap "echo; echo \"Be sure to remove the $test_root directory\"; exit" INT TERM
 
 benchmark_bonnie() {
@@ -31,16 +34,16 @@ benchmark_bonnie() {
 
 benchmark_a_single_file_of_x_gigabytes() {
   size=$1
-  elapsed_sec=$({ TIMEFORMAT=%R; time dd if=/dev/zero of="$test_root/large_file.img" bs=1M count=$[1024*$size] status=none; } 2>&1 )
-  file_size=$(stat --printf="%s" "$test_root/large_file.img")
+  elapsed_sec=$({ TIMEFORMAT=%R; time dd if=/dev/zero of="$test_root/large_file-$size.img" bs=1M count=$[1024*$size] status=none; } 2>&1 )
+  file_size=$(stat --printf="%s" "$test_root/large_file-$size.img")
   write_meg_per_sec=$(echo "scale=2; $file_size / 1024 / 1024 / $elapsed_sec" | bc)
 
-  elapsed_sec=$({ TIMEFORMAT=%R; time cat "$test_root/large_file.img" > /dev/null; } 2>&1 )
-  file_size=$(stat --printf="%s" "$test_root/large_file.img")
+  elapsed_sec=$({ TIMEFORMAT=%R; time cat "$test_root/large_file-$size.img" > /dev/null; } 2>&1 )
+  file_size=$(stat --printf="%s" "$test_root/large_file-$size.img")
   read_meg_per_sec=$(echo "scale=2; $file_size / 1024 / 1024 / $elapsed_sec" | bc)
 
   echo "$runtime,$size,$elapsed_sec,$read_meg_per_sec,$write_meg_per_sec" >> benchmark-single-files.txt
-  rm "$test_root/large_file.img"
+  rm "$test_root/large_file-$size.img"
   sleep 2
 }
 
@@ -50,6 +53,6 @@ benchmark_a_single_file_of_x_gigabytes $[$optimal_file_size / 1024 / 16]
 benchmark_a_single_file_of_x_gigabytes $[$optimal_file_size / 1024 / 8]
 benchmark_a_single_file_of_x_gigabytes $[$optimal_file_size / 1024 / 4]
 benchmark_a_single_file_of_x_gigabytes $[$optimal_file_size / 1024 / 2]
-benchmark_a_single_file_of_x_gigabytes $[$optimal_file_size / 1024]
+benchmark_a_single_file_of_x_gigabytes $[$optimal_file_size / 1024 / 1]
 
 rm -Rf "$test_root"
