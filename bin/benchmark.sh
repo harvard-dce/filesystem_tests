e741c29ede5c1d59665c3009f19c5277172265eb
Dan Collis-Puro <dan@collispuro.net>
2015-08-12 16:11:48 -0400

    Use dd exclusively and test buffered/non-buffered IO ops

--- bin/benchmark.sh
+++ bin/benchmark.sh
@@ -3,10 +3,8 @@
 filesystem="$1"
 parallelism="$2"
 
-files_output="benchmark-files.txt"
+files_output_base="benchmark"
 
-total_ram=$(grep MemTotal /proc/meminfo | awk -F' ' '{print $2}')
-optimal_file_size=$(echo "($total_ram / 1024) * 2" | bc)
 if [ -z "$parallelism" ]; then
   parallelism=3
 fi
@@ -21,8 +19,12 @@ if [ ! -d "$filesystem" ]; then
   exit 1
 fi
 
-if [ ! -e "$files_output" ]; then
-  echo '"runtime_epoch","parallelism","file_size","elapsed_sec","read_meg_per_sec","write_meg_per_sec"' > "$files_output"
+if [ ! -e "${files_output_base}.csv" ]; then
+  echo '"runtime_epoch","parallelism","dd_flags","file_size","elapsed_sec","read_meg_per_sec","write_meg_per_sec"' > "${files_output_base}.csv"
+fi
+
+if [ ! -e "${files_output_base}direct.csv" ]; then
+  echo '"runtime_epoch","parallelism","dd_flags","file_size","elapsed_sec","read_meg_per_sec","write_meg_per_sec"' > "${files_output_base}direct.csv"
 fi
 
 test_root="$filesystem/filesystem_tests_tmp"
@@ -35,24 +37,36 @@ trap "echo; echo \"Be sure to remove the $test_root directory\"; exit" INT TERM
 benchmark_parallel_files_of_x_gigabytes() {
   size=$1
   parallel_actions=$2
+  dd_flags=$3
+
+  if [ ! -z "$dd_flags" ]; then
+    of_dd_flag="oflag=direct"
+    if_dd_flag="iflag=direct"
+  else
+    of_dd_flag=""
+    if_dd_flag=""
+  fi
+
   runtime=$(date +"%s")
-  elapsed_sec=$({ TIMEFORMAT=%R; time seq 1 $parallel_actions | parallel -k "dd if=/dev/zero of=\"$test_root/large_file-{}-$size.img\" bs=1M count=$[1024*$size] status=none"; } 2>&1 )
+  elapsed_sec=$({ TIMEFORMAT=%R; time seq 1 $parallel_actions | parallel -k "dd if=/dev/zero of=\"$test_root/large_file-{}-$size.img\" bs=1M count=$[1024*$size] $of_dd_flag status=none"; } 2>&1 )
   file_size=$(stat --printf="%s" "$test_root/large_file-1-$size.img")
   write_meg_per_sec=$(echo "scale=2; $file_size / 1024 / 1024 / $elapsed_sec" | bc)
 
-  elapsed_sec=$({ TIMEFORMAT=%R; time seq 1 $parallel_actions | parallel -k "cat \"$test_root/large_file-{}-$size.img\" > /dev/null"; } 2>&1 )
+  elapsed_sec=$({ TIMEFORMAT=%R; time seq 1 $parallel_actions | parallel -k "dd if=\"$test_root/large_file-{}-$size.img\" bs=1M status=none $if_dd_flag > /dev/null"; } 2>&1 )
   read_meg_per_sec=$(echo "scale=2; $file_size / 1024 / 1024 / $elapsed_sec" | bc)
 
-  echo "$runtime,$parallelism,$size,$elapsed_sec,$read_meg_per_sec,$write_meg_per_sec" >> "$files_output"
+  echo "$runtime,$parallelism,$dd_flags,$size,$elapsed_sec,$read_meg_per_sec,$write_meg_per_sec" >> "${files_output_base}${dd_flags}.csv"
   rm $test_root/large_file-*-$size.img
   sleep 2
 }
 
-benchmark_parallel_files_of_x_gigabytes 1 $parallelism
-benchmark_parallel_files_of_x_gigabytes $[$optimal_file_size / 1024 / 16] $parallelism
-benchmark_parallel_files_of_x_gigabytes $[$optimal_file_size / 1024 / 8] $parallelism
-benchmark_parallel_files_of_x_gigabytes $[$optimal_file_size / 1024 / 4] $parallelism
-benchmark_parallel_files_of_x_gigabytes $[$optimal_file_size / 1024 / 2] $parallelism
-benchmark_parallel_files_of_x_gigabytes $[$optimal_file_size / 1024 / 1] $parallelism
+benchmark_parallel_files_of_x_gigabytes 1 $parallelism 'direct'
+benchmark_parallel_files_of_x_gigabytes 1 $parallelism ''
+benchmark_parallel_files_of_x_gigabytes 5 $parallelism 'direct'
+benchmark_parallel_files_of_x_gigabytes 5 $parallelism ''
+benchmark_parallel_files_of_x_gigabytes 10 $parallelism 'direct'
+benchmark_parallel_files_of_x_gigabytes 10 $parallelism ''
+benchmark_parallel_files_of_x_gigabytes 20 $parallelism 'direct'
+benchmark_parallel_files_of_x_gigabytes 20 $parallelism ''
 
 rm -Rf "$test_root"
