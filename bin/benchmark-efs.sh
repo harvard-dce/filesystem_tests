630e049e7e5738430ce00c0f69a6b01388ec0eae
Dan Collis-Puro <dan@collispuro.net>
2015-08-18 14:00:12 -0400

    efs tests in parallel

--- bin/benchmark-efs.sh
+++ bin/benchmark-efs.sh
@@ -3,11 +3,11 @@
 source ./bin/common.sh
 
 desired_terabytes=10
-# We're creating 16 gigabyte chunks
-chunks_per_terabyte=64
 
-total_chunks=$[64 * $desired_terabytes]
+# We're creating 16 gigabyte chunks, four at a time
+chunks_per_terabyte=$[1024 / 16 / 4]
+total_chunks=$[$chunks_per_terabyte * $desired_terabytes]
 
 for i in $(seq 1 $total_chunks); do
-  benchmark_parallel_files_of_x_gigabytes 16 1 'direct' 'keep'
+  benchmark_parallel_files_of_x_gigabytes 16 4 'direct' 'keep'
 done;
