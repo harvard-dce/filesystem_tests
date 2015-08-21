43478d4bf797baeee159d3ddb609c87b1012aa99
Dan Collis-Puro <dan@collispuro.net>
2015-08-21 16:17:16 -0400

    2 terabytes per worker

--- bin/benchmark-efs.sh
+++ bin/benchmark-efs.sh
@@ -2,15 +2,12 @@
 
 source ./bin/common.sh
 
-desired_terabytes=1
+desired_terabytes=2
 
 # We're creating 16 gigabyte chunks, 16 at a time
 chunks_per_terabyte=$[1024 / 16 / 16]
 total_chunks=$[$chunks_per_terabyte * $desired_terabytes]
 
-# Only do 512 gb
-total_chunks=$[$total_chunks / 2]
-
 for i in $(seq 1 $total_chunks); do
-  benchmark_parallel_files_of_x_gigabytes 16 16 'direct' 'keep'
+  benchmark_parallel_files_of_x_gigabytes 16 16 '' 'keep'
 done;
