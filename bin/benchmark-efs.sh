eaa8ab880153bf37384d43839785e59593bc30b9
Dan Collis-Puro <dan@collispuro.net>
2015-08-24 10:26:51 -0400

    1TiB at a parallellism of 4, do not keep files

--- bin/benchmark-efs.sh
+++ bin/benchmark-efs.sh
@@ -2,12 +2,12 @@
 
 source ./bin/common.sh
 
-desired_terabytes=2
+desired_terabytes=1
 
-# We're creating 16 gigabyte chunks, 16 at a time
-chunks_per_terabyte=$[1024 / 16 / 16]
+# We're creating 16 gigabyte chunks, 4 at a time
+chunks_per_terabyte=$[1024 / 16 / 4]
 total_chunks=$[$chunks_per_terabyte * $desired_terabytes]
 
 for i in $(seq 1 $total_chunks); do
-  benchmark_parallel_files_of_x_gigabytes 16 16 '' 'keep'
+  benchmark_parallel_files_of_x_gigabytes 16 4 '' ''
 done;
